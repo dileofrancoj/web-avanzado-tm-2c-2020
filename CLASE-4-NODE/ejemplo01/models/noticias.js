ac4baff34e46add1b30beb0624d9b1220c521383
Franco Di Leo <dielo.francoj@gmail.com>
2020-09-22 12:06:37 -0300

    clase 4

--- CLASE-4-NODE/ejemplo01/models/noticias.js
+++ CLASE-4-NODE/ejemplo01/models/noticias.js
@@ -4,6 +4,7 @@ const noticias = [
     titulo: "Nuevo cambio en GITHUB",
     descripcion: "Se reemplazará el nombre de master por main",
     autor: "franco di leo",
+    habilitado: 1,
   },
   {
     id: 2,
@@ -11,10 +12,20 @@ const noticias = [
     descripcion:
       "Nueva versión del framework con novedades de comunicación asincronica :O",
     autor: "Luciano",
+    habilitado: 1,
   },
 ];
 
+const create = (obj) => noticias.push(obj);
+
 const getAll = () => noticias;
 // select * from noticia where id = 1
-const getSingle = (id) => noticias.find((noticia) => noticia.id === id);
-module.exports = { getAll, getSingle };
+const getSingle = (id) =>
+  noticias.find((noticia) => noticia.id == id) || { message: " Not found" };
+
+const del = (id, habilitado = 0) => {
+  const index = noticias.findIndex((noticia) => noticia.id == id); // 1
+  noticias[index]["habilitado"] = habilitado; // noticias[1][]
+};
+
+module.exports = { getAll, getSingle, create, del };
--- CLASE-4-NODE/ejemplo01/routes/noticias.js
+++ CLASE-4-NODE/ejemplo01/routes/noticias.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router(); // PUT, POST, GET, DELETE
-const { getAll, getSingle } = require("./../models/noticias");
+const { getAll, getSingle, create, del } = require("./../models/noticias");
 // /noticias/all
+
+router.get("/single/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const data = getSingle(id);
+  res.json(data);
+});
+
 router.get("/all", (req, res) => {
-  // req -> cabeceras, formulario, params, querystring,sesiones
-  // res -> json, send, render, end
-  const noticias = getAll();
-  res.json(noticias);
+  const data = getAll();
+  res.json(data);
+  res.send();
+});
+
+router.post("/new", (req, res) => {
+  const { body } = req;
+  const data = create(body); // 4
+  res.json({ id: data });
+});
+
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  del(id);
 });
 
 module.exports = router;
